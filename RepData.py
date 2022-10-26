d838b09d515e82603e8ebd3acc3c759c2ef36fc5
nikhitabhatt <115846179+nikhitabhatt@users.noreply.github.com>
2022-10-26 17:32:42 -0400

    Update RepData.py

--- RepData.py
+++ RepData.py
@@ -21,14 +21,22 @@ print('Your house representative is: ' + repName)
 
 #returns rep phone number
 repPhone = response_info['officials'][0]['phones']
-print('Your house representative phone number is: ')
+print(repName + "'s phone number is: ")
 print(repPhone)
 
 #getting the API data (this is for a senator)
 responseSenator = requests.get('https://content-civicinfo.googleapis.com/civicinfo/v2/representatives?address=' + addressString + '&includeOffices=true&levels=country&roles=legislatorUpperBody&key=AIzaSyAehuN2vJJFqQq6rXI_dgHfH6O5at32LW4').text
 responseSenator_info = json.loads(responseSenator)
 
-#returns senator names
+#returns first senator names + numbers
 senator1Name = responseSenator_info['officials'][0]['name']
+senator1Number = responseSenator_info['officials'][0]['phones']
+##returns second senator names + numbers
 senator2Name = responseSenator_info['officials'][1]['name']
+senator2Number = responseSenator_info['officials'][1]['phones']
+
 print('Your senators are: ' + senator1Name + ' and ' + senator2Name)
+print(senator1Name + "'s phone number is: ")
+print(senator1Number)
+print(senator2Name + "'s phone number is: ")
+print(senator2Number)

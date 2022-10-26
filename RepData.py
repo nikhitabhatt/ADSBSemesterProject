d62e63385c25a948c36427843597bb6e79b8f406
nikhitabhatt <115846179+nikhitabhatt@users.noreply.github.com>
2022-10-26 17:18:36 -0400

    Update RepData.py

--- RepData.py
+++ RepData.py
@@ -1,1 +1,34 @@
-print ("hello world")
+import requests
+import json
+
+#Getting user address
+userStreetAddress = input('What is your street address: ')
+userZipcode = input('What is your zipcode: ')
+userCity = input('What is your city: ')
+userState = input('What is your state: ')
+
+##10700 Butterfly Court, 20878, Gaithersburg, MD
+
+addressString = (userStreetAddress + ', ' + userZipcode + ', ' + userCity + ', ' + userState)
+
+#getting the API data (this is for a house rep)
+response = requests.get('https://content-civicinfo.googleapis.com/civicinfo/v2/representatives?address=' + addressString + '&includeOffices=true&levels=country&roles=legislatorLowerBody&key=AIzaSyAehuN2vJJFqQq6rXI_dgHfH6O5at32LW4').text
+response_info = json.loads(response)
+
+#returns rep name
+repName = response_info['officials'][0]['name']
+print('Your house representative is: ' + repName)
+
+#returns rep phone number
+repPhone = response_info['officials'][0]['phones']
+print('Your house representative phone number is: ')
+print(repPhone)
+
+#getting the API data (this is for a senator)
+responseSenator = requests.get('https://content-civicinfo.googleapis.com/civicinfo/v2/representatives?address=' + addressString + '&includeOffices=true&levels=country&roles=legislatorUpperBody&key=AIzaSyAehuN2vJJFqQq6rXI_dgHfH6O5at32LW4').text
+responseSenator_info = json.loads(responseSenator)
+
+#returns senator names
+senator1Name = responseSenator_info['officials'][0]['name']
+senator2Name = responseSenator_info['officials'][1]['name']
+print('Your senators are: ' + senator1Name + ' and ' + senator2Name)
